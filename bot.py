0372a27e838250eae76237e0c1667bfe8758b207
Daniel Wait <71983137+daniel95wait@users.noreply.github.com>
2021-01-04 20:02:19 +0000

    Added project files

---
+++ bot.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+
+import logging
+import os
+import stocks  # stocks.py
+
+
+def install_module(module: str) -> None:
+    import subprocess
+    import sys
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "-U", module])
+
+
+# Downloads modules if not installed
+try:
+    import discord
+    from discord.ext import commands
+except ImportError:
+    install_module("discord.py")
+    import discord
+    from discord.ext import commands
+
+try:
+    from dotenv import load_dotenv
+except ImportError:
+    install_module('python-dotenv')
+    from dotenv import load_dotenv
+
+
+# Logs information and warnings about the bot to a .log file
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s : %(levelname)s : %(name)s : %(message)s'))
+logger.addHandler(handler)
+
+# Fetches bot token from then .env file
+load_dotenv()
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+
+# Initiates bot (prefix comes before any commands)
+bot = commands.Bot(command_prefix="£")
+
+
+@bot.event
+async def on_ready():
+    # Confirmation of log on in console
+    print("You have logged on as {0}".format(bot.user))
+
+
+@bot.command(name="update")
+async def update_stocks(ctx):
+    print("Command: Update stocks")
+    stocks.update_stocks()
+
+
+# Outputs a summary table of the day's stocks
+@bot.command(name="table")
+async def summary_table(ctx):
+    print("Command: Summary table")
+    # Outputs summary table in code block for monospaced font
+    await ctx.channel.send("Summary table:```\n" + stocks.summary_table() + "\n```")
+
+
+# Outputs a graphs for the stocks to a specified number of days (default of 50)
+@bot.command(name="graph")
+async def graph(ctx, days=50):
+    print("Command: Graphs of {0} days".format(days))
+    stocks.update_plots(days)
+
+    stock_images = []
+    for stock_name in stocks.STOCKS_LIST:
+        with open(stocks.graph_path(stock_name) + ".png", "rb") as file:
+            stock_images.append(discord.File(file))
+
+    # Sends all the plots in one message
+    await ctx.channel.send(files=stock_images)
+
+
+bot.run(DISCORD_TOKEN)
