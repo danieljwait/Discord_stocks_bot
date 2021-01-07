6a235d0bc5dab5ceec5bc98b55e19e7b8bfefc1e
Daniel Wait <daniel95_wait@yahoo.co.uk>
2021-01-07 15:57:20 +0000

    Removed extra 'GOTO' from setup.cmd; Bot logging mode switched to 'INFO' from 'DEBUG'

--- bot.py
+++ bot.py
@@ -27,7 +27,7 @@ except ImportError:
 
 # Logs information and warnings about the bot to a .log file
 logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s : %(levelname)s : %(name)s : %(message)s'))
 logger.addHandler(handler)
